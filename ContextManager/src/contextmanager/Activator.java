f76e3ae50e5f077fc204542744d76c16a45704a5
immanuelrhesa <irheza@live.com>
2014-11-20 12:40:11 +0700

    Menambahkan buat ambil data dari sensor...
    tp blom per rentang waktu

--- ContextManager/src/contextmanager/Activator.java
+++ ContextManager/src/contextmanager/Activator.java
@@ -2,15 +2,21 @@ package contextmanager;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import sensor.Sensor;
 
 public class Activator implements BundleActivator {
-
+	 ServiceReference sensorServiceReference;
+	 ContextManagerImpl server;
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		System.out.println("Hello World!!");
+		server = new ContextManagerImpl();
+		
 	}
 	
 	/*
@@ -19,6 +25,16 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext context) throws Exception {
 		System.out.println("Goodbye World!!");
+	}
+	
+	public void getDataSensor(BundleContext context)
+	{
+		
+		sensorServiceReference= context.getServiceReference(Sensor.class.getName());
+	    Sensor sensorService =(Sensor)context.getService(sensorServiceReference);
+	    server.setCuaca(sensorService.getCuaca());
+	    server.setSuhu(sensorService.getSuhu());
+	    server.setTime(sensorService.getTime());
 	}
 
 }
--- ContextManager/src/contextmanager/ContextManagerImpl.java
+++ ContextManager/src/contextmanager/ContextManagerImpl.java
@@ -7,10 +7,28 @@ import entity.PlaceOfInterest;
 
 public class ContextManagerImpl implements ContextManager{
 	Map map = new Map("map.csv");
+	private int suhu;
+	private String cuaca;
+	private String time;
 	@Override
 	public ArrayList<PlaceOfInterest> getCurrentLocationInfo(String location) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void setSuhu(int suhu)
+	{
+		this.suhu = suhu;
+	}
+	
+	public void setCuaca(String cuaca)
+	{
+		this.cuaca=cuaca;
+	}
+	
+	public void setTime(String time)
+	{
+		this.time=time;
 	}
 
 	@Override
